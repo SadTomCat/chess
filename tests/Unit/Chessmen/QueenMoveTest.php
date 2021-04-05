bf192ce97c30b10ea3498fb48e8d6e9637cb5f81
SadTomCat <mrtrevor24@gmail.com>
2021-04-05 22:28:13 +0700

    New: add test for queen

---
+++ tests/Unit/Chessmen/QueenMoveTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Tests\Unit\Chessmen;
+
+use App\Game\GameBoard;
+use PHPUnit\Framework\TestCase;
+
+class QueenMoveTest extends TestCase
+{
+    use HorizontalMoveTest, VerticalMoveTest, DiagonalMoveTest;
+
+    protected string $chessman = 'Q';
+
+    public function test_cannot(): void
+    {
+        $board = GameBoard::createByBoard([
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', '', 'n', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', 'N', '', 'n', '', ''],
+            ['', '', 'N', '', 'Q', 'n', '', ''],
+            ['', '', '', 'N', '', 'n', '', ''],
+            ['', '', '', '', 'N', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+        ]);
+
+        /* white  */
+        $chessman = $board->getChessman(['x' => 4, 'y' => 4]);
+
+        $moveInfo = $chessman->canMove(['x' => 0, 'y' => 0]);
+        $this->assertFalse($moveInfo->getStatus());
+
+        $moveInfo = $chessman->canMove(['x' => 0, 'y' => 4]);
+        $this->assertFalse($moveInfo->getStatus());
+
+        $moveInfo = $chessman->canMove(['x' => 2, 'y' => 6]);
+        $this->assertFalse($moveInfo->getStatus());
+
+        $moveInfo = $chessman->canMove(['x' => 4, 'y' => 6]);
+        $this->assertFalse($moveInfo->getStatus());
+
+        $moveInfo = $chessman->canMove(['x' => 6, 'y' => 6]);
+        $this->assertFalse($moveInfo->getStatus());
+
+        $moveInfo = $chessman->canMove(['x' => 6, 'y' => 4]);
+        $this->assertFalse($moveInfo->getStatus());
+
+        $moveInfo = $chessman->canMove(['x' => 7, 'y' => 1]);
+        $this->assertFalse($moveInfo->getStatus());
+
+        $moveInfo = $chessman->canMove(['x' => 4, 'y' => 2]);
+        $this->assertFalse($moveInfo->getStatus());
+
+        $moveInfo = $chessman->canMove(['x' => 7, 'y' => 2]);
+        $this->assertFalse($moveInfo->getStatus());
+
+        $moveInfo = $chessman->canMove(['x' => 5, 'y' => 7]);
+        $this->assertFalse($moveInfo->getStatus());
+
+        $moveInfo = $chessman->canMove(['x' => 1, 'y' => 3]);
+        $this->assertFalse($moveInfo->getStatus());
+    }
+}
