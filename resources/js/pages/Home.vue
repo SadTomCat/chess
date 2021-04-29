64f22ffd46286c451e8f5b37630f6f211690e010
SadTomCat <mrtrevor24@gmail.com>
2021-04-30 03:23:12 +0700

    Update: add coming soon

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1 class="h-full w-full text-center py-24 text-2xl">{{ $route.name }}</h1>
+    <h1 class="h-full w-full text-center py-24 text-2xl">{{ $route.name }} coming soon</h1>
 </template>
 
 <script>
@@ -7,7 +7,3 @@ export default {
     name: 'Home',
 };
 </script>
-
-<style>
-
-</style>
