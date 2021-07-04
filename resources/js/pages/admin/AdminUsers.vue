c3df666659deb3579317fd2ff6a968b1ed908415
SadTomCat <mrtrevor24@gmail.com>
2021-07-04 20:23:41 +0700

    Update add new columns in AdminUsers

--- resources/js/pages/admin/AdminUsers.vue
+++ resources/js/pages/admin/AdminUsers.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="admin-users px-10 py-10 flex justify-center">
-        <api-table :columns="['id', 'name', 'email']"
+        <api-table :columns="['id', 'name', 'email', 'blocked', 'blocked_at', 'role']"
                    :table="'users'"
                    :default-actions="['view']"
         ></api-table>
